2cac9f367aedf001a2d7e734b388532505ea4f9d
Todd Rylaarsdam <todd@toddr.org>
2022-06-07 09:08:41 -0500

    updates

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from 'emotion';
-import { locationService } from "@grafana/runtime"
+import { locationService } from '@grafana/runtime';
 import { stylesFactory } from '@grafana/ui';
 import CSS from 'csstype';
 interface Props extends PanelProps<SimpleOptions> {}
@@ -12,10 +12,10 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
   const styles = getStyles();
   const leftStyle: CSS.Properties = {
     float: 'left',
-  }
+  };
   const rightStyle: CSS.Properties = {
     float: 'right',
-  }
+  };
   return (
     <div
       className={cx(
@@ -30,24 +30,34 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
         <tbody>
           <tr>
             <td width="50%">
-              <button onClick={() => {
-                console.log(locationService.getLocation())
-                locationService.partial({"from": "1594671549256"}, true)
-                locationService.partial({"to": "1644671549256"}, true)
-                locationService.reload()
-                // locationService.push({"from": "1594671549256", "to": "1644671549256", "orgId": 1})
-
-              }} style={leftStyle} className="css-1a8393j-button">Previous Interval</button>
+              <button
+                onClick={() => {
+                  console.log(locationService.getLocation());
+                  locationService.partial({ from: '1594671549256' }, true);
+                  locationService.partial({ to: '1644671549256' }, true);
+                  locationService.reload();
+                  // locationService.push({"from": "1594671549256", "to": "1644671549256", "orgId": 1})
+                }}
+                style={leftStyle}
+                className="css-1a8393j-button"
+              >
+                Previous Interval
+              </button>
             </td>
             <td width="50%">
-              <button onClick={() => {
-                console.log(locationService.getLocation())
-                locationService.partial({"from": "1594671549256"}, true)
-                locationService.partial({"to": "1644671549256"}, true)
-                locationService.reload()
-                // locationService.push({"from": "1594671549256", "to": "1644671549256", "orgId": 1})
-
-              }} style={rightStyle} className="css-1a8393j-button">Next Interval</button>
+              <button
+                onClick={() => {
+                  console.log(locationService.getLocation());
+                  locationService.partial({ from: '1594671549256' }, true);
+                  locationService.partial({ to: '1644671549256' }, true);
+                  locationService.reload();
+                  // locationService.push({"from": "1594671549256", "to": "1644671549256", "orgId": 1})
+                }}
+                style={rightStyle}
+                className="css-1a8393j-button"
+              >
+                Next Interval
+              </button>
             </td>
           </tr>
         </tbody>
